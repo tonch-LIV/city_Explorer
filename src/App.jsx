3b78be46fc950280d97701368235ee8e98bcd5bf
Antonio Garcia Pardo <garcia54.a@gmail.com>
2026-05-21 11:59:30 -0700

    basic structure and setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import axios from "axios";
 import bootstrap from 'bootstrap'; 'react-bootstrap';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
